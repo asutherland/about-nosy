8af414147048c913704856b63ba8744203f5226d
Andrew Sutherland <asutherland@asutherland.org>
2012-01-28 21:44:42 -0800

    end of day status commit; window, compartment processing logic

---
+++ data/lib/aboutnosy/memfrobrep.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Processes the serialized format of `memfrob.js` into a more consumer-friendly
+ *  representation.
+ **/
+
+define(
+  [
+    'exports'
+  ],
+  function(
+    exports
+  ) {
+
+}); // end define
